cfe883a73a89e4c9ddabd0f1e35e1bfe8d677483
Felix Jordan <felix.jordan@buddy-app.de>
2023-05-27 21:25:37 +0200

    Add website backup

---
+++ precache-manifest.ea078ca9765643a0dd246ef4323ce8de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7910a8a63ad3d94f7f02b922acb196c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43293b7a7d0dfac3c75f",
+    "url": "/static/css/2.fa903d0a.chunk.css"
+  },
+  {
+    "revision": "56e30b26c6229d74745f",
+    "url": "/static/css/main.9f18931a.chunk.css"
+  },
+  {
+    "revision": "43293b7a7d0dfac3c75f",
+    "url": "/static/js/2.7709e6f2.chunk.js"
+  },
+  {
+    "revision": "00d0c8170027f558ccbe3fc5eeee5e24",
+    "url": "/static/js/2.7709e6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e30b26c6229d74745f",
+    "url": "/static/js/main.bba170c9.chunk.js"
+  },
+  {
+    "revision": "32cf661040ef1887245a",
+    "url": "/static/js/runtime-main.385270e4.js"
+  }
+]);
